79a7a9ff067af6d634cf34db11cc663fab5233ae
pat-white-code <pat.k.white@gmail.com>
2020-04-28 20:47:20 -0500

    FIX LISTING JS

---
+++ src/components/classes/classList.js
@@ -0,0 +1,18 @@
+// TODO: Create a class component container for Listing component for ComponentDidMount and componentDidUpdate.
+
+// class classList extends Component {
+//   state = {  }
+//   componentDidMout(){
+//     props.getUserBusinesses(props.userId)
+//   }
+//   // componentDidUpdate(prev){
+//   //   props.getUserBusinesses(props.userId)
+//   // }
+//   render() { 
+//     return ( 
+//       <Listing />
+//     );
+//   }
+// }
+
+// export default classList;
