ee666d02df2d679a846bbd372295f13b621969bb
pat-white-code <pat.k.white@gmail.com>
2020-05-03 17:27:52 -0500

    adds more temporary stored businesses in redux state

--- src/redux/state.js
+++ src/redux/state.js
@@ -11,6 +11,22 @@ const state = {
       description: 'Hip spot for coffee, beer & pastries in wood-paneled digs, plus tables & a taco truck out back.',
       hours: '10 AM - 12AM',
       website: 'https://www.radiocoffeeandbeer.com/'
+    },
+    {
+      tempId: 2,
+      name: 'Summer Moon Coffee',
+      address: '3115 S 1st St #1b, Austin, TX 78704',
+      description: 'Local roaster for wood-fired, fair-trade coffee, pastries & breakfast tacos in a quaint setting.',
+      hours: '6 AM - 12 AM',
+      website: 'https://summermooncoffee.com/'
+    },
+    {
+      tempId: 3,
+      name: 'Brew & Brew',
+      address: '500 San Marcos St #105, Austin, TX 78702',
+      description: 'Bar & cafe pairing coffee & craft brews with sandwiches & light bites in stylish industrial digs.',
+      hours: '8 AM - 8 PM',
+      website: 'https://thebrewandbrew.com/'
     }
   ]
 }
